dd63df9b9d166b42820f30ce639eb0b93e674a78
Just-Hope <46832701+Just-Hope@users.noreply.github.com>
2019-02-26 12:08:09 -0500

    Update Checkers_template.js

--- Checkers_template.js
+++ Checkers_template.js
@@ -1,27 +1,39 @@
 
-function cell(rowNum, cellNum) {
-    return `<div id="cell-1-1" class="cell red"></div>`
-}
-
-var rowTemplate =`
-${cell(1,1)}
-<div id="cell-1-2" class="cell black" onclick="toggle(this)">
-    <div class="checker red-checker"></div>         
-    <div class="checker black-checker" hidden></div>   
-</div>
-${cell(1,3)}
-<div id="cell-1-4" class="cell black" onclick="toggle(this)">
-    <div class="checker red-checker"></div>
-    <div class="checker black-checker" hidden></div>
-</div>
-${cell(1,5)}
-<div id="cell-1-6" class="cell black" onclick="toggle(this)">
-    <div class="checker red-checker"></div>
-    <div class="checker black-checker" hidden></div>
-</div>
-${cell(1,7)}
-<div id="cell-1-8" class="cell black" onclick="toggle(this)">
-    <div class="checker red-checker"></div>
-    <div class="checker black-checker" hidden></div>
+function cellTemplate(rowNum, cellNum) {
+  
+    var isCellEven = true
+    var isRowEven = true
+    if (cellNum % 2 == 1) isCellEven = false
+    if (rowNum % 2 == 1) isRowEven = false
+    
+    if (isCellEven != isRowEven) {
+        // Black cell
+return `<div id="cell-${rowNum}-${cellNum}" class="cell black" onclick="toggle(this)">
+<div class="checker red-checker"></div>
+<div class="checker black-checker" hidden></div>
 </div>
 `
+    } else
+    //white cell
+    return `<div id="cell-${rowNum}-${cellNum}" class="cell red"></div>`
+}
+
+
+function rowTemplate(rowNum) {
+    console.log(`drawing row number ${rowNum}`)
+    var rowString = ''
+    rowString = rowString + `<div id="row-{rowNum}" class="row" >`
+    for (var cellNum=1; cellNum<=8; cellNum++) {
+        rowString = rowString + cellTemplate(rowNum, cellNum)
+    }
+    rowString = rowString + `</div>`
+    return rowString
+}
+
+function boardTemplate() {
+    var boardString = ''
+    for (var rowNum=1; rowNum<=8; rowNum++) {
+        boardString = boardString + rowTemplate(rowNum)
+    }
+    return boardString
+}
