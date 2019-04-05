4894c812373aa210454da69fcd703eb705413b52
Just-Hope <46832701+Just-Hope@users.noreply.github.com>
2019-04-05 16:46:45 -0400

    Done with Video 10

--- Checkers_template.js
+++ Checkers_template.js
@@ -27,43 +27,6 @@ var checkers = [
     {row: 8, cell: 7, color: 'black'},
 ]
 
-function renderBoard() {
-    return `
-    ${renderRow(1)}
-    ${renderRow(2)}
-    ${renderRow(3)}
-    ${renderRow(4)}
-    ${renderRow(5)}
-    ${renderRow(6)}
-    ${renderRow(7)}
-    ${renderRow(8)}
-    `
-}
-
-function renderRow(rowNum) {
-    return `
-    <div id="row-${rowNum}" class="row">
-    ${renderCell(rowNum, 1)}
-    ${renderCell(rowNum, 2)}
-    ${renderCell(rowNum, 3)}
-    ${renderCell(rowNum, 4)}
-    ${renderCell(rowNum, 5)}
-    ${renderCell(rowNum, 6)}
-    ${renderCell(rowNum, 7)}
-    ${renderCell(rowNum, 8)}
-    </div>
-    `
-}
-
-function renderCell(rowNum, cellNum) {
-    if (cellColor(rowNum, cellNum) === 'black') { 
-return `<div id="cell-${rowNum}-${cellNum}" class="cell black"> </div>`
-} else
-    return `<div id="cell-${rowNum}-${cellNum}" class="cell white"></div>`
-}
-
-
-
 function renderCheckers(){
     console.log('rendering checkers')
     for(let i=0; i<checkers.length; i++) {
@@ -73,23 +36,12 @@ function renderCheckers(){
     }
 }
 
-
 function renderChecker(color) {
     return `<div class="checker ${color}-checker"></div>`
 }
 
-function parity(num) {
-    if (num % 2 === 0) return 'even'
-    else return 'odd'
-}
-
-function cellColor(rowNum, cellNum) {
-    if(parity (rowNum) === parity(cellNum)) return 'white'
-    else return 'black'
-}
-
-
 function selectChecker(checker) {
+    let checker = $(this).children().first()
     console.log('selecting checker: ', checker)
+    selectedChecker = checker
 }
-
